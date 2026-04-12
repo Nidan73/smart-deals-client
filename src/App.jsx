09307c5e656261c439878e60e3e7cf99593f09e6
Nidan73 <Nidanalam73@gmail.com>
2026-04-12 21:33:20 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center">
+        <p>My name is nidan</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
